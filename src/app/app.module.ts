5763a4da236556b3aa1a243b3e36a3fb58645d54
Dante Lacey-Thompson <dlacey-thompson@csumb.edu>
2018-12-23 12:43:26 -0800

    added header component for application with responsive material navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule} from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { MatToolbarModule, MatMenuModule, MatIconModule, MatListModule, MatSidenavModule, MatButtonModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    AppHeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatMenuModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
